97391ae8d16d1fb382d0e086ddf407a28b9cff5c
zhun185 <15086880628@163.com>
2025-05-16 21:38:54 +0800

    更新IndexPage.vue

--- mstar-classification-frontend/src/pages/IndexPage.vue
+++ mstar-classification-frontend/src/pages/IndexPage.vue
@@ -3,6 +3,16 @@
     <div class="text-center">
       <h1 class="text-h3">M-Star 分类系统</h1>
       <p class="text-subtitle1">欢迎使用基于光谱和图像数据的M型星分类工具。</p>
+      <div class="q-my-md">
+        <p>
+          GitHub 仓库:
+          <a href="https://github.com/zhun185/Star_Classfication" target="_blank">https://github.com/zhun185/Star_Classfication</a>
+        </p>
+        <p>
+          相关论文:
+          <a href="https://iopscience.iop.org/article/10.1088/1538-3873/acc7ca" target="_blank">https://iopscience.iop.org/article/10.1088/1538-3873/acc7ca</a>
+        </p>
+      </div>
       <q-btn color="primary" label="开始探索" to="/data" />
     </div>
   </q-page>
